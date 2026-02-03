d36443f4327943c479a9a429987a6c06e834ca6b
Süheyla CAN <suheylacan@192.168.1.122>
2026-02-03 13:37:28 +0300

    add reaction timer project

---
+++ src/components/AppResults.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props : ['score'],
+    data () {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = "Super fast"
+        }
+        else if (this.rank < 400) {
+            this.rank = "Rapid reflexes"
+        }
+        else {
+            this.rank = "Snail pace.."
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
